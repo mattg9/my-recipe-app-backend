35749ea259d3db3bc75c1f8de8bd050ed0dcbfee
Matthew Gaston <matthew.gaston@broadcom.com>
2024-05-26 16:18:27 -0400

    Add initial migration for recipes app, requirements, and redirect localhost to a relevant view

--- recipe_app_backend/urls.py
+++ recipe_app_backend/urls.py
@@ -1,8 +1,11 @@
 # recipe_app_backend/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('recipes.urls')),
+    path('', RedirectView.as_view(url='/api/recipes/', permanent=True)),
 ]
