9a1e46a215d86dffbb612645903ff18f61fc2374
Matthew Gaston <matthew.gaston@broadcom.com>
2024-05-27 22:06:26 -0400

    django-filter

--- recipe_app_backend/settings.py
+++ recipe_app_backend/settings.py
@@ -40,7 +40,12 @@ INSTALLED_APPS = [
     'recipes',
     'rest_framework',
     'corsheaders',
+    'django_filters',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,8 @@
 # recipes/views.py
 from rest_framework import generics
-from rest_framework import status
-from rest_framework.response import Response
 from .models import Recipe
 from .serializers import RecipeSerializer
 
 class RecipeListCreateView(generics.ListCreateAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-
-    def search_recipes(request):
-        query = request.GET.get('query', '')
-        if not query:
-            return Response({"detail": "Please provide a search query"}, status=status.HTTP_400_BAD_REQUEST)
-        results = Recipe.objects.filter(title__icontains=query)
-        serialized_results = RecipeSerializer(results, many=True).data
-        return Response(serialized_results)
