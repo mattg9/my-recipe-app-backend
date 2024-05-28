52c8e69d3a76bde8eb60f65d0259c22f33ae5362
Matthew Gaston <matthew.gaston@broadcom.com>
2024-05-27 21:01:51 -0400

    search get request

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,20 @@
 # recipes/views.py
 from rest_framework import generics
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Recipe
 from .serializers import RecipeSerializer
-from django.http import JsonResponse
 
 class RecipeListCreateView(generics.ListCreateAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
 
+@api_view(['GET'])
 def search_recipes(request):
-    query = request.GET.get('query')
-    results = Recipe.objects.filter(name__icontains=query)
-    serialized_results = [recipe.serialize() for recipe in results]
-    return JsonResponse(serialized_results, safe=False)
+    query = request.GET.get('query', '')
+    if not query:
+        return Response({"detail": "Please provide a search query"}, status=status.HTTP_400_BAD_REQUEST)
+    results = Recipe.objects.filter(title__icontains=query)
+    serialized_results = RecipeSerializer(results, many=True).data
+    return Response(serialized_results)
