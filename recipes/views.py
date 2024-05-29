1149da864b1b9c091a33b2653242b7cc14f9ae6b
Matthew Gaston <matthew.gaston@broadcom.com>
2024-05-28 21:04:01 -0400

    filter and searching

--- recipes/views.py
+++ recipes/views.py
@@ -2,9 +2,11 @@ from rest_framework import generics
 from .models import Recipe
 from .serializers import RecipeSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 class RecipeListView(generics.ListCreateAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
-    filter_backends = (DjangoFilterBackend,)
-    filter_fields = ('title',)
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['title']
+    search_fields = ['ingredients', 'instructions']
