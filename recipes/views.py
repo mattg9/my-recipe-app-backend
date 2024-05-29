772d830ae59485b4d76142ccc87003371f24c6ed
Matthew Gaston <matthew.gaston@broadcom.com>
2024-05-28 21:16:23 -0400

    pagination limit offset

--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,16 @@ from .models import Recipe
 from .serializers import RecipeSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework.pagination import LimitOffsetPagination
+
+class RecipePagination(LimitOffsetPagination):
+    default_limit = 5
+    max_limit = 100
 
 class RecipeListView(generics.ListCreateAPIView):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
+    pagination_class = RecipePagination
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
     filterset_fields = ['title']
     search_fields = ['ingredients', 'instructions']
