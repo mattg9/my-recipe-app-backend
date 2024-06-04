870f367e7a395bed51066e0ff3d950b514dfcdf2
Matthew <matthewgaston9@gmail.com>
2024-06-03 23:26:06 -0400

    My first testcase

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .models import Recipe
 
-# Create your tests here.
+class RecipeCreateTestCase(APITestCase):
+
+    def test_create_recipe(self):
+        recipe_count = Recipe.objects.count()
+        recipe_attrs = {
+            'title': 'Beef Stew',
+            'ingredients': 'Beef\nCarrots\nPotatoes\nStock',
+            'instructions': 'Preheat slow cooker\nPut everything in the pot\nCook for 30 minutes',
+            'prep_time': '00:05:00',
+            'cook_time': '00:30:00'
+        }
+        response = self.client.post('/api/recipes/', recipe_attrs)
+        if (response.status_code != 201):
+            print(response.status_code)
+            print(response.data)
+        self.assertEqual(
+            Recipe.objects.count(),
+            recipe_count + 1,
+        )
+        for attr, expected_value in recipe_attrs.items():
+            self.assertEqual(response.data[attr], expected_value)
+        self.assertEqual(response.data['servings'], 1)
