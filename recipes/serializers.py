2d10fc622d0f4808d212906dbca22b0fb7a45b8c
Matthew <matthewgaston9@gmail.com>
2024-06-03 21:53:14 -0400

    remove photo from the Serializer

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -3,8 +3,8 @@ from .models import Recipe
 
 class RecipeSerializer(serializers.ModelSerializer):
 
-    photo = serializers.ImageField(default=None)
+    # photo = serializers.ImageField(default=None)
     class Meta:
         model = Recipe
-        fields = ['id', 'title', 'ingredients', 'instructions', 'prep_time', 'cook_time',
-                  'servings', 'photo']
+        fields = ['id', 'title', 'ingredients', 'instructions', 
+                  'prep_time', 'cook_time', 'servings']
--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import RecipeCreateView, RecipeRetrieveUpdateDestroy
 
 urlpatterns = [
-    path('recipes/', RecipeCreateView.as_view(), name='recipe-list-create'),
-    path('recipes/<int:pk>/', RecipeRetrieveUpdateDestroy.as_view(), name='recipe-retrieve-update-destroy'),
+    path('recipes/', RecipeCreateView.as_view(), 
+         name='recipe-list-create'),
+    path('recipes/<int:pk>/', RecipeRetrieveUpdateDestroy.as_view(), 
+         name='recipe-retrieve-update-destroy'),
 ]
