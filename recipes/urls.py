f2bf6ccc5af7e104713db58be4de9d9b68df4110
Matthew Gaston <matthew.gaston@broadcom.com>
2024-06-07 22:27:10 -0400

    trailing slash

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import RecipeCreateView, RecipeRetrieveUpdateDestroy
 
 urlpatterns = [
-    path('recipes', RecipeCreateView.as_view(), 
+    path('recipes/', RecipeCreateView.as_view(), 
          name='recipe-list-create'),
-    path('recipes/<int:pk>', RecipeRetrieveUpdateDestroy.as_view(), 
+    path('recipes/<int:pk>/', RecipeRetrieveUpdateDestroy.as_view(), 
          name='recipe-retrieve-update-destroy'),
 ]
